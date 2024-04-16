90849cc3869cfd08a990ec9d665e0cb03de2e0e8
ryou0507 <ryouji57@gmail.com>
2024-04-16 18:11:08 +0900

    統合2

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <footer>
         <div class="container1">
             <p>〇事業概要</p>
@@ -35,42 +34,4 @@
     <?php wp_footer(); ?>
 </body>
 
-=======
-<footer>
-        <div class="container1">
-            <p>〇事業概要</p>
-            <ul>
-                <li><a href="#flow">制作依頼の流れ</a></li>
-                <li><a href="pricing.html">料金表</a></li>
-                <li><a href="sample.html">サンプル一覧</a></li>
-                <li><a href="FAQ.html">よくある質問</a></li>
-            </ul>
-        </div>
-
-        <div class="container2">
-            <p>〇運営情報</p>
-            <ul>
-                <li><a href="info.html">運営者概要</a></li>
-                <li><a href="#お知らせのリンク">お知らせ</a></li>
-                <li><a href="policy.html">プライバシーポリシー</a></li>
-            </ul>
-        </div>
-
-        <div class="container3">
-            <p>〇お客様ページ</p>
-            <ul>
-                <li><a href="#コラムのリンク">お役立ち情報</a></li>
-                <li><a href="inquiry.html">お問い合わせ</a></li>
-            </ul>
-        </div>
-    </footer>
-
-
-
-
-
-    <?php wp_footer(); ?>
-</body>
-
->>>>>>> e0358ea7b3a353e8356b2a3280100540e9e9f1c3
 </html>

bb90564ddf9a3d3612427fa010ae7afeccb46ed4
ryou0507 <ryouji57@gmail.com>
2024-05-09 19:52:53 +0900

    修正統合
    フッターなど

--- functions.php
+++ functions.php
@@ -70,4 +70,3 @@ if ( function_exists( 'add_theme_support' ) ) {
     add_theme_support( 'post-thumbnails' );
 }
 
-
--- single-post.php
+++ single-post.php
@@ -23,54 +23,54 @@
                     ?>
                 </main><!-- #main -->
             </div><!-- #primary -->
+        </div>
+        <!-- 関連記事 -->
+        <aside class="related-post-wrap">
 
-            <!-- 関連記事 -->
-            <aside class="related-post-wrap">
+            <h3>関連記事</h3>
 
-                <h3>関連記事</h3>
+            <ul>
+                <?php
+                $catkwds = array();
 
-                <ul>
-                    <?php
-                    $catkwds = array();
+                if (has_category()) {
 
-                    if (has_category()) {
+                    $cats = get_the_category();
 
-                        $cats = get_the_category();
-
-                        foreach ($cats as $cat) {
-                            $catkwds[] = $cat->term_id;
-                        }
+                    foreach ($cats as $cat) {
+                        $catkwds[] = $cat->term_id;
                     }
-                    $args = array(
-                        'post_type' => 'post',
-                        'posts_per_page' => '4',
-                        'post__not_in' => array($post->ID),
-                        'category__in' => $catkwds,
-                        'orderby' => 'rand'
-                    );
-                    $the_query = new WP_Query($args);
-                    if ($the_query->have_posts()) :
-                        while ($the_query->have_posts()) : $the_query->the_post();
-                    ?>
-                            <li class="related-post">
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php
-                                    if (has_post_thumbnail()) {
+                }
+                $args = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => '4',
+                    'post__not_in' => array($post->ID),
+                    'category__in' => $catkwds,
+                    'orderby' => 'rand'
+                );
+                $the_query = new WP_Query($args);
+                if ($the_query->have_posts()) :
+                    while ($the_query->have_posts()) : $the_query->the_post();
+                ?>
+                        <li class="related-post">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php
+                                if (has_post_thumbnail()) {
 
-                                        the_post_thumbnail('medium');
-                                    } else {
+                                    the_post_thumbnail('medium');
+                                } else {
 
-                                        echo '<img src="' . get_template_directory_uri() . '/images/no-image.gif">';
-                                    }
-                                    ?>
-                                    <div class="text-wrap">
-                                        <?php the_title(); ?>
-                                    </div>
-                                </a>
-                            </li>
-                    <?php
-                        endwhile;
-                    endif;
+                                    echo '<img src="' . get_template_directory_uri() . '/images/no-image.gif">';
+                                }
+                                ?>
+                                <div class="text-wrap">
+                                    <?php the_title(); ?>
+                                </div>
+                            </a>
+                        </li>
+                <?php
+                    endwhile;
+                endif;
 
                     wp_reset_postdata();
                     ?>
